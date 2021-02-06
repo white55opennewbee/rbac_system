823dbac8daa8c67b02f5b6970c5e7769fc4c92bc
white55opennewbee <291217616@qq.com>
2021-02-06 21:35:30 +0800

    "设置es地址端口为阿里云es"

--- src/main/java/com/pc/rbac_system/config/ElasticSearchConfig.java
+++ src/main/java/com/pc/rbac_system/config/ElasticSearchConfig.java
@@ -3,16 +3,21 @@ package com.pc.rbac_system.config;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElasticSearchConfig {
+    @Value("${esHost}")
+    private String esHost;
+    @Value("${esPort}")
+    private Integer esport;
     @Bean
     public RestHighLevelClient elasticsearchRestTemplate(){
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(
-                        new HttpHost("127.0.0.1", 9200, "http")));
+                        new HttpHost(esHost, esport, "http")));
         return client;
 
     }
